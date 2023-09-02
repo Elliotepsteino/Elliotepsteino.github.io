4ce18eaaf60d49891d198eae4bb1912e3bebb545
Elliot Epstein <elliotepstein14@gmail.com>
2023-09-02 10:02:59 -0700

    Improve Favicon design

--- _make_favicon.sh
+++ _make_favicon.sh
@@ -1,1 +1,1 @@
-convert -background transparent "images/EE_bold.png" -define icon:auto-resize=48,96,144,192 "favicon.ico"
+convert -background transparent "images/EE_bold_curved.png" -define icon:auto-resize=48,96,144,192 "favicon.ico"
